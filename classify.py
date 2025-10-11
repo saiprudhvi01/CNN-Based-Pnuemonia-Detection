cb039841ec02d18b65dc215f171cf71307ab1094
saiprudhvi01 <saiprudhvibodempudi11@gmail.com>
2025-10-11 18:43:37 +0530

    Remove duplicate classify function to fix NoneType model error

--- classify.py
+++ classify.py
@@ -118,26 +118,6 @@ def skin_net(img):
         'seborrheic keratosis', 'dermatofibroma', 'nevus'
     ][np.argmax(result)]
 
-def classify(img):
-    im = cv2.resize(img, (52, 52))
-    result = cls_model.predict(np.array([im]))
-    classification = np.argmax(result)
-    
-    if classification == 0:
-        return "Enter a valid medical image"
-    elif classification == 1:
-        return bone_net(im)
-    elif classification == 2:
-        return brain_net(im)
-    elif classification == 3:
-        return eye_net(im)
-    elif classification == 4:
-        return kidney_net(im)
-    elif classification == 5:
-        return chest_net(im)
-    elif classification == 6:
-        return skin_net(im)
-
 def bone_net(img):
     result = fract_model.predict(np.array([img]))
     return ['not fractured', 'fractured'][np.argmax(result)]
